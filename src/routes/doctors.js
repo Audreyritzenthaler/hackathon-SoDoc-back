148ed3e2de902b9535cc3ec9a24d33bd7281c53a
Miura <tadamiura@gmail.com>
2020-06-25 16:52:21 +0200

    get doctor profile, doctor patients, all message from a patient

--- src/routes/doctors.js
+++ src/routes/doctors.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { connection } = require('../../config')
 const Router = express.Router()
 
+//GET messages by doctor by mood_status 0 by unread 
 Router.get('/:id/messages', (req, res) => {
 	const idDoctor = req.params.id
     const sql ='SELECT  message, patient.id, patient.firstname, patient.lastname, mood_status, creation_date FROM message JOIN patient ON patient.id = message.patient_id JOIN doctor ON doctor.id = message.doc_id WHERE 1 = 1 AND doctor.id = ? AND message.mood_status = 0 AND message.read=false ORDER BY creation_date DESC, patient.id DESC'
@@ -12,6 +13,49 @@ Router.get('/:id/messages', (req, res) => {
 		} else {
 			return res.status(200).json(result)
 		}
+	})
+})
+
+//GET all messages of a patient for a doctor
+Router.get('/:id/patients/messages', (req, res) => {
+    const idDoctor = req.params.id
+    const idPatient = req.query.id
+    const sql ='SELECT message, mood_status, creation_date, message.read FROM message JOIN patient ON patient.id = message.patient_id JOIN doctor ON doctor.id = message.doc_id WHERE 1=1 AND doctor.id = ? AND patient.id = ? ORDER BY creation_date DESC'
+
+	connection.query(sql, [idDoctor, idPatient], (err, result) => {
+		if (err) {
+			return res.status(500).send(`An error occurred: ${err.message}`)
+		} else {
+			return res.status(200).json(result)
+		}
+	})
+})
+
+
+//GET doctor's patients 
+Router.get('/:id/patients', (req,res) => {
+    const idDoctor = req.params.id
+    const idPatient = req.query.id
+    const sql = 'SELECT DISTINCT p.firstname, p.lastname, p.email, p.phoneNumber FROM message m JOIN patient p ON p.id = m.patient_id JOIN doctor d ON m.doc_id = d.id   '
+    connection.query(sql, [idDoctor,idPatient], (err,result) => {
+        if (err) {
+			return res.status(500).send(`An error occurred: ${err.message}`)
+		} else {
+			return res.status(200).json(result)
+		}
+    })
+})
+
+//GET doctor's profile
+Router.get('/:id', (req, res) => {
+	const sql = 'SELECT firstname, lastname, email, phoneNumber FROM doctor'
+
+		connection.query(sql, (err,result) => {
+			if (err) {
+		return res.status(500).send(`An error occurred: ${err.message}`)
+	} else {
+		return res.status(200).json(result)
+	}
 	})
 })
 
