9108f9c97d6ef515d7393f25adf2474f206783d7
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-06-25 18:31:44 +0200

    transform from body to query

--- src/routes/patients.js
+++ src/routes/patients.js
@@ -6,7 +6,7 @@ const Router = express.Router()
 Router.get('/:id/messages', (req, res) => {
   const values = [
     patientId = req.params.id,
-    doctorId = req.body.doctorId
+    doctorId = req.query.doctorId
   ]
   const sql = 'SELECT message.message, message.creation_date, message.mood_status FROM message JOIN patient ON message.patient_id = patient.id JOIN doctor ON message.doc_id = doctor.id WHERE 1 = 1 AND patient.id = ? AND doctor.id = ? ORDER BY creation_date DESC'
 
